5056c22d0c9b7935f4d22d68734e25349698c12c
Jonny <Jonny@310f1995-8640-0410-8131-c34790f6e5a4>
2007-12-06 18:55:40 +0000

    Rename grade to track_grade
    git-svn-id: file:///Volumes/Jonny/svnroot/tracks/trunk@5 310f1995-8640-0410-8131-c34790f6e5a4

---
+++ test/unit/track_grade_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class TrackGradeTest < Test::Unit::TestCase
+  fixtures :track_grades
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
