60916c406e81267653742e5644cfd2883ca38972
Jonny <Jonny@310f1995-8640-0410-8131-c34790f6e5a4>
2007-12-06 18:37:01 +0000

    Rename status to track_access
    git-svn-id: file:///Volumes/Jonny/svnroot/tracks/trunk@3 310f1995-8640-0410-8131-c34790f6e5a4

---
+++ test/unit/track_access_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class TrackAccessTest < Test::Unit::TestCase
+  fixtures :track_accesses
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
