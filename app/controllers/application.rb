aa4a58dbbb979be00da17e8a910a3978e1facf5a
Jonny <Jonny@310f1995-8640-0410-8131-c34790f6e5a4>
2007-12-11 17:21:27 +0000

    Upgrade to 2.0.1
    git-svn-id: file:///Volumes/Jonny/svnroot/tracks/trunk@20 310f1995-8640-0410-8131-c34790f6e5a4

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -5,5 +5,5 @@ class ApplicationController < ActionController::Base
   include AuthenticatedSystem
 
   # Pick a unique cookie name to distinguish our session data from others'
-  session :session_key => '_Tracks_session_id'
+  #session :session_key => '_Tracks_session_id'
 end
