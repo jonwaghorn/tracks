c99afb7c6f322ca26f4e211d4a35c618583ef07e
Jon Waghorn <jon@youdo.co.nz>
2009-06-14 06:31:21 -0700

    Get tests to a not failing state

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -5,6 +5,7 @@ class UserTest < Test::Unit::TestCase
   # Then, you can remove it from this and the functional test.
   include AuthenticatedTestHelper
   fixtures :users
+  fixtures :map_types
 
   def test_should_create_user
     assert_difference User, :count do
@@ -34,25 +35,25 @@ class UserTest < Test::Unit::TestCase
     end
   end
 
-  def test_should_require_email
+  def test_should_require_login
     assert_no_difference User, :count do
-      u = create_user(:email => nil)
-      assert u.errors.on(:email)
+      u = create_user(:login => nil)
+      assert u.errors.on(:login)
     end
   end
 
   def test_should_reset_password
     users(:quentin).update_attributes(:password => 'new password', :password_confirmation => 'new password')
-    assert_equal users(:quentin), User.authenticate('quentin', 'new password')
+    assert_equal users(:quentin), User.authenticate('quentin@example.com', 'new password')
   end
 
   def test_should_not_rehash_password
-    users(:quentin).update_attributes(:login => 'quentin2')
-    assert_equal users(:quentin), User.authenticate('quentin2', 'test')
+    users(:quentin).update_attributes(:login => 'quentin2@example.com')
+    assert_equal users(:quentin), User.authenticate('quentin2@example.com', 'test')
   end
 
   def test_should_authenticate_user
-    assert_equal users(:quentin), User.authenticate('quentin', 'test')
+    assert_equal users(:quentin), User.authenticate('quentin@example.com', 'test')
   end
 
   def test_should_set_remember_token
@@ -70,6 +71,6 @@ class UserTest < Test::Unit::TestCase
 
   protected
     def create_user(options = {})
-      User.create({ :login => 'quire', :email => 'quire@example.com', :password => 'quire', :password_confirmation => 'quire' }.merge(options))
+      User.create({ :screen_name => 'quire', :login => 'quire@example.com', :password => 'quire', :password_confirmation => 'quire' }.merge(options))
     end
 end
