49fffd2d8ebec2102b593d3e7a28df3e44aa0896
Jon Waghorn <jon.waghorn@gmail.com>
2011-08-13 14:34:09 +1200

    Rmagick nonsense

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,8 @@
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
+require 'RMagick'
+
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
